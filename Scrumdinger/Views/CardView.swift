d23d2f061546ef2ac8a347017b4c5ed3be70a746
Yu Wakui <wak121.y@gmail.com>
2024-06-08 11:55:38 +0900

    feat: Add accessibility labels to attendees count and lengthInMinutes labels in CardView
    - Added accessibility trait  to the title Text view for better VoiceOver navigation
    - Added accessibilityLabel to the attendees count Label to announce the number of attendees
    - Added accessibilityLabel to the lengthInMinutes Label to announce the length of the meeting

--- Scrumdinger/Views/CardView.swift
+++ Scrumdinger/Views/CardView.swift
@@ -14,11 +14,14 @@ struct CardView: View {
         VStack(alignment: .leading) {
             Text(scrum.title)
                 .font(.headline)
+                .accessibilityAddTraits(.isHeader)
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count)")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minutes metting")
                     .labelStyle(.trailingIcon)
             }
             .font(.caption)
