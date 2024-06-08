e8aaddc54942b2e250b98eb598c55d1963b50b8e
Yu Wakui <wak121.y@gmail.com>
2024-06-08 12:01:57 +0900

    feat: Implement ScrumsView to display a list of DailyScrums

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,25 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Yu Wakui on 2024/06/08.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
