7ea904311ae662d5c8fba58401af335ef5da1610
Yu Wakui <wak121.y@gmail.com>
2024-06-08 13:52:12 +0900

    feat: Implemented DetailView, updated DailyScrum and Theme models

---
+++ Scrumdinger/Views/DetailView.swift
@@ -0,0 +1,57 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Yu Wakui on 2024/06/08.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            // Meeting info section
+            Section(header: Text("Meeting Info")) {
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4.0)
+                }
+                .accessibilityElement(children: .combine)
+            }
+            
+            // Attendees section
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }
+        .navigationTitle(scrum.title)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
