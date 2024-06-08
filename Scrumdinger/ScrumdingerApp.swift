be3c19f8c1b87f9f6265c27fef615dabc2758cd6
Yu Wakui <wak121.y@gmail.com>
2024-06-08 09:27:11 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Yu Wakui on 2024/06/08.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
