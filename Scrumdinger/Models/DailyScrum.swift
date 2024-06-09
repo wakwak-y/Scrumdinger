d21f3efc5374366f1a35b2eb856a6b0fdc0cad39
Yu Wakui <wak121.y@gmail.com>
2024-06-09 18:16:35 +0900

    feat: Make DailyScrum.Attendee, History, and Theme conform to Codable
    - Updated DailyScrum.Attendee to conform to Codable for encoding and decoding support.
    - Updated History to conform to Codable for encoding and decoding support.
    - Updated Theme to conform to Codable for encoding and decoding support.

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -34,7 +34,7 @@ struct DailyScrum: Identifiable {
 }
 
 extension DailyScrum {
-    struct Attendee: Identifiable {
+    struct Attendee: Identifiable, Codable {
         let id: UUID
         var name: String
         
